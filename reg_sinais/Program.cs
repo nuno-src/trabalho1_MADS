6b2ddfcadce9b0ddeedf094a9285ead91f24a203
nuno <75589883+nuno-src@users.noreply.github.com>
2023-04-13 01:52:41 +0100

    initial commit

---
+++ reg_sinais/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reg_sinais
+{
+    internal class Program
+    {
+        private List<sinal> lista_sinais = new List<sinal>();
+        static void Main(string[] args)
+        {
+
+            //private List<sinal> lista_sinais = new List<sinal>();
+
+
+            int op = 9;
+            while (op != 0)
+            {
+                Console.WriteLine("\n-------------------MENU-----------------");
+                Console.WriteLine(" 1) - Adicionar sinal");
+                
+                
+                Console.WriteLine(" 0) - Sair");
+                Console.WriteLine("------------------------------------------");
+
+
+                Console.Write("Escolha uma opção:");
+
+                op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+
+                    //case 1: Remover_recluso(); break;
+                    
+                    case 0: Environment.Exit(0); break;
+
+
+
+
+                    default:
+                        Console.WriteLine("OPÇÃO ERRADA");
+                        break;
+
+
+                }
+
+            }
+
+
+
+
+
+
+        }
+
+
+
+        static void Adicionar_sinal()
+        { 
+        
+        
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ reg_sinais/sinal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reg_sinais
+{
+    internal class sinal
+    {
+        private int id;
+        private int codigo;
+        private int numero; // apenas para sinais com numeros
+        private int referencia;
+
+        /*
+         
+            Acerca de cada sinal vertical (não luminoso) guarda-se a seguinte informação (estática):
+                identificador único no sistema da empresa;
+                uma referência para o código de sinal (disponível em
+                https://servicos.infraestruturasdeportugal.pt/pt-pt/parceiros/normas-desinalizacao/sinais-de-alerta);
+                valor nomimal (no sinais com números);
+                referência ao identificador da EN ou toponímia;
+                data inicial - data de colocação no terreno (ou na falta, data do primeiro
+                levantamento);
+                data fim - data quando o sinal foi removido/perdido.
+
+
+            Acerca de cada sinal vertical (não luminoso) guarda-se a seguinte informação
+            temporal, ou seja, associada a cada levantamento ou vistoria:
+                referência à vistoria;
+                índice de qualidade [0..3];
+                perdido/removido (V/F).
+
+            Uma vistoria, tem:
+                um identificador único;
+                uma data (dia único);
+                a equipa de pessoas que a compunha
+         
+         
+         */
+
+
+
+
+
+    }
+}
